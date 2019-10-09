c89fc174da51a63c40c1699c17f3212b2143c8eb
Gizzlow <jdtrujilloj@uqvirtual.edu.co>
2019-10-08 20:09:18 -0500

    1.1.1

--- FrontEnd/src/app/app.service.ts
+++ FrontEnd/src/app/app.service.ts
@@ -1,1 +1,18 @@
 import { Injectable } from '@angular/core';
+
+@Injectable()
+export class InventarioService {
+
+    const{
+
+    }
+
+    export interface Producto {
+
+        nombre: string;
+        descripcion: string;
+        foto
+
+
+    }
+}
