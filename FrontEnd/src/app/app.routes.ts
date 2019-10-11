7a5d3eef604c50f13afae21630dd3bd2a9851326
Gizzlow <jdtrujilloj@uqvirtual.edu.co>
2019-10-10 22:22:43 -0500

    1.2.0

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -5,8 +5,8 @@ import {NavigationComponent} from './navigation/navigation.component';
 import {MenuComponent} from './menu/menu.component';
 import {CalculadoraComponent} from './calculadora/calculadora.component';
 import {InventarioComponent} from './inventario/inventario.component';
-import { LoginGuard } from './login.guard';
-import { NoLoginGuard } from './no-login.guard';
+import {LoginGuard} from './login.guard';
+import {NoLoginGuard} from './no-login.guard';
 import {ListaComponent} from './usuarios/lista/lista.component';
 import {RegistrarComponent} from './usuarios/registrar/registrar.component';
 
@@ -17,8 +17,8 @@ const ROUTES: Routes = [
     { path: 'transacciones', component: MenuComponent, canActivate: [LoginGuard]},
     { path: 'herramientas', component: CalculadoraComponent, canActivate: [LoginGuard]},
     { path: 'inventario', component: InventarioComponent, canActivate: [LoginGuard]},
-    { path: 'usuarios', component: ListaComponent},
-	{ path: 'registar', component: RegistrarComponent},
+    { path: 'usuarios', component: ListaComponent, canActivate: [LoginGuard]},
+	{ path: 'registrar', component: RegistrarComponent, canActivate: [LoginGuard]},
     { path: '**', pathMatch: 'full', redirectTo: 'login'},
 
 ];
